7499d8b2b48d210158be67f44b03dd10eb537aff
Liliia <rudenkoliliia13@gmail.com>
2025-04-09 23:13:21 +0100

    Remove TryVersion folder from project

--- TryVersion/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        // Create and launch GUI, or do whatever your main method needs.
-        GUI gui = new GUI();
-    }
-}
